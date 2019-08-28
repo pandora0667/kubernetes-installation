477f16165442fef3a19084874b42c70413aff1d7
Seongwon Lee <seongwon@edu.hanbat.ac.kr>
2019-08-28 18:53:52 +0900

    kunernetes-master shell change

--- kubernetes-master.sh
+++ kubernetes-master.sh
@@ -4,14 +4,14 @@ echo "Hello Seongwon Kubernetes master installation shell script. for ubuntu 18.
 echo "Kubernets master will be installed in 5 seconds. Press Ctrl + C to cancel."
 
 sleep 5 
-echo "installation update and package" 
+echo "---------- installation update and package ----------" 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.old && sudo sed -i 's/archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
 sudo dpkg-reconfigure tzdata
 sudo apt apt update && sudo apt upgrade -y 
 sudo apt remove docker docker-engine docker.io containerd runc -y 
 sudo apt install git nmap htop glances wget curl apt-transport-https ca-certificates gnupg-agent software-properties-common -y 
 
-echo "docker installation\n" 
+echo "---------- docker installation ----------" 
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -26,12 +26,13 @@ sudo docker --version
 sudo systemctl start docker
 sudo systemctl enable docker
 
-echo "docker installation complete." 
-echo "Kubernetes installation start." 
+echo "---------- docker installation complete. ----------" 
+echo "---------- Kubernetes installation start. ----------" 
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo swapoff -a
+sudo apt install kubeadm -y
 sudo kubeadm version
 sudo kubeadm init --pod-network-cidr=172.168.10.0/24 > ~/join-command.txt 
 
@@ -39,11 +40,11 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "kubernetes installation complete confirm to nodes\n" 
+echo "---------- kubernetes installation complete confirm to nodes ----------" 
 
 sudo kubectl get nodes
 
-echo "Pod Network from Master node and verify pod namespaces.\n" 
+echo "---------- Pod Network from Master node and verify pod namespaces. ----------" 
 
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
