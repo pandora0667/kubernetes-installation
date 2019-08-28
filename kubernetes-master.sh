410a8877b681e3e5d161fe78849a99ba925e98cd
Seongwon Lee <seongwon@edu.hanbat.ac.kr>
2019-08-28 17:01:57 +0900

    master update

--- kubernetes-master.sh
+++ kubernetes-master.sh
@@ -1,12 +1,12 @@
 #/bin/sh
 
-echo "Hello Seongwon Kubernetes master installation shell script. for ubuntu 18.04 LTS Version\n" 
-echo "Kubernets master will be installed in 5 seconds. Press Ctrl + C to cancel.\n"
+echo "Hello Seongwon Kubernetes master installation shell script. for ubuntu 18.04 LTS Version" 
+echo "Kubernets master will be installed in 5 seconds. Press Ctrl + C to cancel."
 
 sleep 5 
-echo "installation update and package\n" 
+echo "installation update and package" 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.old && sudo sed -i 's/archive.ubuntu.com/ftp.daumkakao.com/g' /etc/apt/sources.list
-sudo dkkg-reconfigure tzdata
+sudo dpkg-reconfigure tzdata
 sudo apt apt update && sudo apt upgrade -y 
 sudo apt remove docker docker-engine docker.io containerd runc -y 
 sudo apt install git nmap htop glances wget curl apt-transport-https ca-certificates gnupg-agent software-properties-common -y 
@@ -26,8 +26,8 @@ sudo docker --version
 sudo systemctl start docker
 sudo systemctl enable docker
 
-echo "docker installation complete\n" 
-ehco "Kubernetes installation start\n" 
+echo "docker installation complete." 
+echo "Kubernetes installation start." 
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
@@ -50,7 +50,7 @@ sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Do
 sudo  kubectl get nodes
 sudo  kubectl get pods --all-namespaces
 
-echo "Congratulations, the kubernets master installation is complete. Please check the following command and run the kubernetes-slave.sh file.\n" 
+echo "Congratulations, the kubernets master installation is complete. Please check the following command and run the kubernetes-slave.sh file." 
 sleep 2
 
 cat ~/join-command.txt
